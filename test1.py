b46fbcd59405edcabfafc74bc0419be353ecbb44
rafaelvalotta <rafavalotta@gmail.com>
2025-04-03 13:47:56 -0400

    another change

--- test1.py
+++ test1.py
@@ -1,18 +1,20 @@
+
+import py_wake
 import matplotlib.pyplot as plt
 
+from py_wake.examples.data.hornsrev1 import Hornsrev1Site, V80, wt_x, wt_y, wt16_x, wt16_y
+from py_wake import NOJ
+
+windTurbines = V80()
+site = Hornsrev1Site()
+noj = NOJ(site,windTurbines)
+simulationResult = noj(wt_x,wt_y)
+aep_result = simulationResult.aep().sum()
+
+plt.figure()
+plt.scatter(wt_x,wt_y, marker = '2')
+plt.show()
+
+print('AEP:', aep_result)
+
 print('done')
-
-print('done again')
-
-print('done even again')
-
-print('done even again for a second time')
-print('done even again for a third time')
-print('done even again for a fourth time, 1:49pm')
-print('done even again for a fifth time, 1:52pm')
-print('done even again for a sixth time, 1:53pm')
-print('done even again for a seventh time, 2:03pm')
-print('done even again for a eightieth time, 9:40am')
-
-
-
